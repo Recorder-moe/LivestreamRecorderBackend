32bff8c299a536b5be521b76be4bf5b82ac6ce1f
陳鈞 <jim60105@gmail.com>
2023-07-31 17:29:59 +0800

    chore(logging): Add Enrich.WithMachineName and level switch

--- Helper/Logger.cs
+++ Helper/Logger.cs
@@ -1,5 +1,6 @@
 ﻿using Newtonsoft.Json;
 using Serilog;
+using Serilog.Core;
 using Serilog.Events;
 using System;
 using System.Linq;
@@ -29,16 +30,19 @@ public static class Log
     {
         Serilog.Debugging.SelfLog.Enable(msg => Console.WriteLine(msg));
 
+        var levelSwitch = new LoggingLevelSwitch(LogEventLevel.Information);
+
         var logger = new LoggerConfiguration()
-                        .MinimumLevel.Verbose()
+                        .MinimumLevel.ControlledBy(levelSwitch)
                         .MinimumLevel.Override("Microsoft", LogEventLevel.Fatal)
                         .MinimumLevel.Override("Microsoft.Hosting.Lifetime", LogEventLevel.Fatal)
                         .MinimumLevel.Override("System", LogEventLevel.Fatal)
                         .WriteTo.Console(outputTemplate: "{Timestamp:HH:mm:ss} [{Level:u3}] {Message:lj} <{SourceContext}>{NewLine}{Exception}",
-                                         restrictedToMinimumLevel: LogEventLevel.Verbose)
+                                         theme: Serilog.Sinks.SystemConsole.Themes.AnsiConsoleTheme.Code)
                         .WriteTo.Seq(serverUrl: Environment.GetEnvironmentVariable("Seq_ServerUrl")!,
                                      apiKey: Environment.GetEnvironmentVariable("Seq_ApiKey"),
-                                     restrictedToMinimumLevel: LogEventLevel.Verbose)
+                                     controlLevelSwitch: levelSwitch)
+                        .Enrich.WithMachineName()
                         .Enrich.FromLogContext()
                         .CreateLogger();
         return logger;
